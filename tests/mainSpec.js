f15180fb438571ac42108e8c19f22f27613d40a7
ButenkoT <tanya@butenko.me>
2014-12-09 16:59:12 +1100

    jasmine

--- tests/mainSpec.js
+++ tests/mainSpec.js
@@ -1,3 +1,29 @@
-/**
- * Created by tetianabutenko on 24/11/2014.
- */
+describe('welcome.js spec', function () {
+  var welcome;
+
+  beforeEach(function () {
+    welcome = new app.Views.WelcomePage();
+  });
+
+  describe('when welcome-view is constructing', function () {
+
+    it('should exist', function () {
+      expect(welcome).toBeDefined();
+    });
+
+  });
+
+  describe('when welcome-view is rendered', function () {
+
+    beforeEach(function () {
+      welcome.render();
+    });
+
+    it ('should create welcomeForm', function () {
+      expect(welcome.$el).toBe('div');
+    });
+
+  });
+
+});
+
